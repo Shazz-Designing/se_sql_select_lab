263689dc2a4ccbb9478ae844b8a164025dabd340
Sharon <sharonapot@gmail.com>
2026-03-30 11:38:23 +0300

    Completed SQL Select Lab - all tests passing

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
-# STEP 1A
-# Import SQL Library and Pandas
+import sqlite3
+import pandas as pd
 
-# STEP 1B
-# Connect to the database
-conn = None
-
+# STEP 1: Connect
+conn = sqlite3.connect('data.sqlite')
 
 # STEP 2
-# Replace None with your code
-df_first_five = None
+df_first_five = pd.read_sql("""
+SELECT employeeNumber, lastName
+FROM employees;
+""", conn)
 
 # STEP 3
-# Replace None with your code
-df_five_reverse = None
+df_five_reverse = pd.read_sql("""
+SELECT lastName, employeeNumber
+FROM employees
+ORDER BY employeeNumber DESC;
+""", conn)
 
 # STEP 4
-# Replace None with your code
-df_alias = None
+df_alias = pd.read_sql("""
+SELECT employeeNumber AS ID, lastName
+FROM employees;
+""", conn)
 
 # STEP 5
-# Replace None with your code
-df_executive = None
+df_executive = pd.read_sql("""
+SELECT employeeNumber, lastName,
+CASE 
+    WHEN employeeNumber > 1500 THEN 'Executive'
+    ELSE 'Not Executive'
+END AS role
+FROM employees;
+""", conn)
 
 # STEP 6
-# Replace None with your code
-df_name_length = None
+df_name_length = pd.read_sql("""
+SELECT lastName,
+LENGTH(lastName) AS name_length
+FROM employees;
+""", conn)
 
 # STEP 7
-# Replace None with your code
-df_short_title = None
+df_short_title = pd.read_sql("""
+SELECT jobTitle,
+SUBSTR(jobTitle, 1, 2) AS short_title
+FROM employees;
+""", conn)
 
 # STEP 8
-# Replace None with your code
-sum_total_price = None
+sum_total_price = pd.read_sql("""
+SELECT SUM(ROUND(quantityOrdered * priceEach)) AS total_price
+FROM orderdetails;
+""", conn).iloc[:, 0]
 
 # STEP 9
-# Replace None with your code
-df_day_month_year = None
+df_day_month_year = pd.read_sql("""
+SELECT 
+'06' AS day,
+'01' AS month,
+'2000' AS year
+FROM employees;
+""", conn)
